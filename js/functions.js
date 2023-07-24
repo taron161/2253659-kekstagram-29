b0b0e2c35f3f0ff3b457f870de49bbd2cb19dbaf
Taron Mesropyan <krastiart@gmail.com>
2023-07-24 10:45:16 +0300

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,11 @@
 const lengthTxt = 18;
 const txt = 'Проверка на колическтво символов';
 
-function isValid (str, lengthStr) {
-  return str.length <= lengthStr;
-}
+const isValid = (str, lengthStr) => str.length <= lengthStr;
 
 isValid(txt ,lengthTxt);
 
-function isPalindrome (str) {
+const isPalindrome = (str) => {
   let newStr = str.toLowerCase();
   str = newStr;
   newStr = str.replaceAll(' ', '');
@@ -18,6 +16,41 @@ function isPalindrome (str) {
     }
   }
   return true;
-}
+};
 
 isPalindrome(txt);
+
+// функция вычисляет, укладывается ли встреча в рабочее время или нет
+/*
+'startDay' - начало рабочего дня
+'endDay' - конец рабочего дня
+'meetingStart' - начало встречи
+'meetingDuration' - продолжительность встречи в минутах
+*/
+const isDuringBusinessHours = (startDay, endDay, meetingStart, meetingDuration) => {
+  let tmp = startDay;
+  startDay = tmp.split(':');
+
+  tmp = endDay;
+  endDay = tmp.split(':');
+
+  tmp = meetingStart;
+  meetingStart = tmp.split(':');
+
+  const startDayInMinutes = Number(startDay[0]) * 60 + Number(startDay[1]);
+
+  const endDayInMinutes = Number(endDay[0]) * 60 + Number(endDay[1]);
+
+  const meetingStartInMinutes = Number(meetingStart[0]) * 60 + Number(meetingStart[1]);
+
+  if (startDayInMinutes <= meetingStartInMinutes && meetingStartInMinutes <= endDayInMinutes) {
+    const timeLeft = endDayInMinutes - meetingStartInMinutes;
+
+    if (timeLeft >= meetingDuration) {
+      return true;
+    }
+  }
+  return false;
+};
+
+isDuringBusinessHours('09:20', '17:00', '16:30', 30);
