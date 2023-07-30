94117265e3b631bb2a644e87a29b5e0564742e38
Taron Mesropyan <krastiart@gmail.com>
2023-07-30 17:21:30 +0300

    Открывается и закрывается (часть 2)

---
+++ js/comments.js
@@ -0,0 +1,56 @@
+const modal = document.querySelector('.big-picture');
+
+
+const createCommentsFragment = (comments, maxCount, minCount) => {
+  const commentsFragment = document.createDocumentFragment();
+  const commentsPart = comments.slice(minCount, maxCount);
+  commentsPart.forEach(({ avatar, message, name, id }) => {
+    const comment = document.createElement('li');
+    comment.classList.add('social__comment');
+
+    const img = document.createElement('img');
+    img.alt = name;
+    img.src = avatar;
+    img.width = 35;
+    img.height = 35;
+    img.classList.add('social__picture');
+    comment.append(img);
+
+    const commentMessage = document.createElement('p');
+    commentMessage.classList.add('social__text');
+    commentMessage.textContent = message;
+    comment.append(commentMessage);
+
+    comment.setAttribute('data-id', id);
+    commentsFragment.append(comment);
+  });
+  return commentsFragment;
+};
+
+// Создает поля с комментариями
+const addComments = (comments, minCount, maxCount) => {
+  if (maxCount > comments.length) {
+    maxCount = comments.length;
+  }
+  const commentContainer = modal.querySelector('.social__comments');
+  commentContainer.append(createCommentsFragment(comments, maxCount, minCount));
+};
+
+// Проверяет, больше ли общее количество комментариев, чем
+// показанное. Если да, то оставляет кнопку загрузки новых комментариев,
+// если нет, скрывает ее и приравнивает значения
+const checkQuantityOfComment = (totalCommentsCount, currentCommentsCount) => {
+  const currentCountElement = modal.querySelector('.comments-current');
+  const loaderButton = modal.querySelector('.comments-loader');
+
+  if (totalCommentsCount <= currentCommentsCount) {
+    currentCommentsCount = totalCommentsCount;
+    loaderButton.classList.add('hidden');
+  } else if (loaderButton.classList.contains('hidden')) {
+    loaderButton.classList.remove('hidden');
+  }
+
+  currentCountElement.textContent = currentCommentsCount;
+};
+
+export { addComments, checkQuantityOfComment };
