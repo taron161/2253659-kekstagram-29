43a6a89e1e8d545abd426c9d45a90a4db6b9fb89
Taron Mesropyan <krastiart@gmail.com>
2023-08-05 06:23:11 +0300

    Изменяет изображения фильров на выбранный файл

--- js/upload-image.js
+++ js/upload-image.js
@@ -1,13 +1,20 @@
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
-const getImage = (input, preview) => {
+const getImage = (input, image) => {
   const file = input.files[0];
   const fileName = file.name.toLowerCase();
 
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
 
   if (matches) {
-    preview.src = URL.createObjectURL(file);
+    const imageUrl = URL.createObjectURL(file);
+    image.src = imageUrl;
+
+    const smallImages = document.querySelectorAll('.effects__preview');
+
+    smallImages.forEach((item) => {
+      item.style.backgroundImage = `url(${imageUrl})`;
+    });
   }
 };
 
