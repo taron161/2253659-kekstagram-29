d6183728c8b211daa6697d9b995fd25a4b551e3d
Taron <115931492+taron161@users.noreply.github.com>
2024-02-27 23:38:58 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = ''https://29.javascript.htmlacademy.pro/kekstagram';
+const BASE_URL = 'https://29.javascript.htmlacademy.pro/kekstagram';
 const Route = {
   GET_DATA: '/data',
   SEND_DATA: '/',
